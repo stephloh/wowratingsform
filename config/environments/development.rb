e53fd90e49edf7c94ede60a13be16c5bca54785c
Stephanie L <kystephanieloh@hotmail.com>
2014-02-26 22:01:30 +0800

    Commit two

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Wowform2::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -37,5 +37,5 @@ Wowform2::Application.configure do
 
   # Required for Devise gem
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  
+
 end
